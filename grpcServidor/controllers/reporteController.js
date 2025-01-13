2980a6a661c7997ffd60683ea42fb3c6809e60dd
Fernando-G3 <nando2015.jfgo@gmail.com>
2025-01-13 15:58:37 -0600

    se cambio el ip adress

--- grpcServidor/controllers/reporteController.js
+++ grpcServidor/controllers/reporteController.js
@@ -1,12 +1,11 @@
-const { consultarExperienciasCompletasPorUsuario } = require('../serviciosNode/DataAccessObjects/ExperienciaEducativaDAO');
-const { generarReporteKardex } = require('../utils/generadorReporte');
+//const { consultarExperienciasCompletasPorUsuario } = require('../serviciosNode/DataAccessObjects/ExperienciaEducativaDAO');
+//const { generarReporteKardex } = require('../utils/generadorReporte');
 
 const ReportService = {
   GenerarReporteKardex: async (call, callback) => {
     const { idUsuario } = call.request;
 
     try {
-      // Valida que el idUsuario sea válido
       if (!idUsuario) {
         return callback({
           code: grpc.status.INVALID_ARGUMENT,
@@ -14,7 +13,6 @@ const ReportService = {
         });
       }
 
-      // Obtén las experiencias educativas del usuario
       const experiencias = await consultarExperienciasCompletasPorUsuario(idUsuario);
      
       if (!experiencias || experiencias.length === 0) {
@@ -23,14 +21,10 @@ const ReportService = {
           message: "No se encontraron experiencias educativas para este usuario",
         });
       }
-      
-      // Simula los datos del usuario (puedes ajustarlo según tu lógica)
+    
       const usuario = { nombre: `Usuario ${idUsuario}` };
-
-      // Genera el reporte
       const buffer = await generarReporteKardex(experiencias, usuario);
 
-      // Responde con la ruta del archivo
       callback(null, {
         message: "Reporte generado con éxito",
         file: buffer.toString('base64'),
